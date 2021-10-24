d8fe32d5d0996b25ab470b4b79fc0720b600e948
borshon420 <borshonarafat420@gmail.com>
2021-10-25 02:09:19 +0600

    react car service mongo crud

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css';
+
+const AddService = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+      console.log(data);
+      axios.post('http://localhost:5000/services', data)
+      .then(res => {
+          console.log(res)
+          if(res.data.insertedId){
+              alert('added successfully')
+              reset();
+          }
+      })
+    };
+    return (
+        <div className="add-service mt-5">
+            <h1>Please Add a Server</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name"/>
+      <textarea {...register("description")} placeholder="Description"/>
+      <input type="number" {...register("price")} placeholder="Price"/>
+      <input {...register("img")} placeholder="Img url"/>
+      <input type="submit" />
+    </form>
+        </div>
+    );
+};
+
+export default AddService;
---
+++ src/Pages/Booking/Booking/Booking.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const Booking = () => {
+    const {serviceId} = useParams(); 
+    const [service, setService] = useState({});
+    useEffect(()=> {
+        fetch(`http://localhost:5000/services/${serviceId}`)
+        .then(res => res.json())
+        .then(data => setService(data))
+    }, [])
+    return (
+        <div className="mt-5">
+            <h2>Details of: {service.name}</h2>
+            <h1>This is booking {serviceId}</h1>
+        </div>
+    );
+};
+
+export default Booking;
